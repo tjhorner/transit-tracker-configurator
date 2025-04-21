d6524dee79d337557be8b8063b309f10f545f89c
TJ Horner <me@tjhorner.com>
2025-04-21 10:30:37 -0700

    actually check colors properly lol

--- src/lib/components/RouteStopChooser.svelte
+++ src/lib/components/RouteStopChooser.svelte
@@ -85,12 +85,10 @@
   }
 
   function colorIsDark(color: string) {
-    const r = parseInt(color.slice(1, 3), 16)
-    const g = parseInt(color.slice(3, 5), 16)
-    const b = parseInt(color.slice(5, 7), 16)
-
-    const lum = 0.2126 * r + 0.7152 * g + 0.0722 * b
-    return lum < 64
+    const r = parseInt(color.slice(0, 2), 16)
+    const g = parseInt(color.slice(2, 4), 16)
+    const b = parseInt(color.slice(4, 6), 16)
+    return Math.max(r, g, b) < 80
   }
 
   const onMapMoved = debounce(_onMapMoved, 1000)
